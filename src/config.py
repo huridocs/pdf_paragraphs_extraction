1b52777070314a44295d522954dcfbba576686d6
Gabo <gabriel.piles.glez@gmail.com>
2022-12-21 12:27:02 +0100

    Run formatter

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import graypy
 
-GRAYLOG_IP = os.environ.get('GRAYLOG_IP')
+GRAYLOG_IP = os.environ.get("GRAYLOG_IP")
 REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
 REDIS_PORT = os.environ.get("REDIS_PORT", "6739")
 MONGO_HOST = os.environ.get("MONGO_HOST", "localhost")
@@ -17,13 +17,7 @@ ROOT_PATH = Path(__file__).parent.parent.absolute()
 
 handlers = [logging.StreamHandler()]
 if GRAYLOG_IP:
-    handlers.append(graypy.GELFUDPHandler(
-        GRAYLOG_IP, 12201, localname="convert-to-pdf"
-    ))
+    handlers.append(graypy.GELFUDPHandler(GRAYLOG_IP, 12201, localname="convert-to-pdf"))
 
 logging.root.handlers = []
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=handlers
-)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", handlers=handlers)
