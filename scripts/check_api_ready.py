5110f5a51fb58fa35dc50c53dee7c7daea656b72
Gabo <gabriel.piles.glez@gmail.com>
2022-12-22 15:47:24 +0100

    Handle connection error in check api

--- scripts/check_api_ready.py
+++ scripts/check_api_ready.py
@@ -5,11 +5,12 @@ import requests
 if __name__ == "__main__":
     service_url = "http://localhost:5051/info"
 
-    for i in range(30):
+    for i in range(60):
         time.sleep(0.5)
-        response = requests.get(service_url)
-        if response.status_code == 200:
-            print("ready!")
-            break
-
-        print("not ready, yet")
+        try:
+            response = requests.get(service_url)
+            if response.status_code == 200:
+                print("ready!")
+                break
+        except requests.exceptions.ConnectionError:
+            print("not ready, yet")
