8e535428d08e4d0550e2f3d75e983e24ab41e02c
Gabo <gabriel.piles.glez@gmail.com>
2022-12-22 15:41:44 +0100

    Run formatter

--- scripts/check_api_ready.py
+++ scripts/check_api_ready.py
@@ -2,14 +2,14 @@ import time
 
 import requests
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     service_url = "http://localhost:5051/info"
 
     for i in range(30):
         time.sleep(0.5)
         response = requests.get(service_url)
         if response.status_code == 200:
-            print('ready!')
+            print("ready!")
             break
 
-        print('not ready, yet')
+        print("not ready, yet")
