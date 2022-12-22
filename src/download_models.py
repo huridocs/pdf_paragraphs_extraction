57a8763f05595116b6f4b5623c2888ea5c49585a
Gabo <gabriel.piles.glez@gmail.com>
2022-12-22 12:35:47 +0100

    Use downloaded models

--- src/download_models.py
+++ src/download_models.py
@@ -2,35 +2,35 @@ from huggingface_hub import hf_hub_download
 
 import config
 
-hf_hub_download(
+segmentation_model_config_path = hf_hub_download(
     repo_id="HURIDOCS/pdf-segmetation",
     filename="segmentator_model_config.txt",
     revision="52c44568f40ba3c19bd22e5ebc363425b7130a6b",
     cache_dir=config.HUGGINGFACE_PATH,
 )
 
-hf_hub_download(
+segmentation_model_path = hf_hub_download(
     repo_id="HURIDOCS/pdf-segmetation",
     filename="segmentator_model.txt",
     revision="ab83ee7d75e7e1cfe7f0a740d1bf6a3b74a1fdf3",
     cache_dir=config.HUGGINGFACE_PATH,
 )
 
-hf_hub_download(
+tag_type_finding_config_path = hf_hub_download(
     repo_id="HURIDOCS/pdf-segmetation",
     filename="tag_type_finding_model_config.txt",
     revision="7d98776dd34acb2fe3a06495c82e64b9c84bdc16",
     cache_dir=config.HUGGINGFACE_PATH,
 )
 
-hf_hub_download(
+pdf_tag_type_model_path = hf_hub_download(
     repo_id="HURIDOCS/pdf-segmetation",
     filename="tag_type_finding_model.txt",
     revision="c9e886597823a7995a1454f2de43b821bc930368",
     cache_dir=config.HUGGINGFACE_PATH,
 )
 
-hf_hub_download(
+letter_corpus_path = hf_hub_download(
     repo_id="HURIDOCS/pdf-segmetation",
     filename="letter_corpus.txt",
     revision="da00a69c8d6a84493712e819580c0148757f466c",
--- src/extract_pdf_paragraphs/segmentator/predict.py
+++ src/extract_pdf_paragraphs/segmentator/predict.py
@@ -2,10 +2,7 @@ import ast
 from typing import Dict
 
 import lightgbm as lgb
-
-from huggingface_hub import hf_hub_download
-
-import config
+from download_models import segmentation_model_config_path, segmentation_model_path
 from extract_pdf_paragraphs.segmentator.LightGBM_30Features_TagType_OneHotTwoLetter_LightGBM_30Features import (
     LightGBM_30Features_TagType_OneHotTwoLetter_LightGBM_30Features,
 )
@@ -20,21 +17,7 @@ def get_model_configs(config_path: str) -> Dict:
 
 
 def predict(pdf_features):
-    model_config_path = hf_hub_download(
-        repo_id="HURIDOCS/pdf-segmetation",
-        filename="segmentator_model_config.txt",
-        revision="52c44568f40ba3c19bd22e5ebc363425b7130a6b",
-        cache_dir=config.HUGGINGFACE_PATH,
-    )
-
-    model_configs: {} = get_model_configs(model_config_path)
-
-    segmentation_model_path = hf_hub_download(
-        repo_id="HURIDOCS/pdf-segmetation",
-        filename="segmentator_model.txt",
-        revision="ab83ee7d75e7e1cfe7f0a740d1bf6a3b74a1fdf3",
-        cache_dir=config.HUGGINGFACE_PATH,
-    )
+    model_configs: {} = get_model_configs(segmentation_model_config_path)
 
     lightgbm_model = lgb.Booster(model_file=segmentation_model_path)
     lightgbm_segmentator = LightGBM_30Features_TagType_OneHotTwoLetter_LightGBM_30Features(
