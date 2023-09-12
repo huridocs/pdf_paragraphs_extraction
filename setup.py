2d6828131f8df1eb5a52884ff5b69fd36983a2d9
Ali Altıparmak <ali@huridocs.org>
2023-09-12 17:37:07 +0300

    Fix setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=["paragraph_extraction_trainer"],
     package_dir={"": "src"},
     version="0.11",
-    url="https://github.com/huridocs/pdf_paragraphs_extraction/tree/use-pdftohtml",
+    url="https://github.com/huridocs/pdf_paragraphs_extraction",
     author="HURIDOCS",
     description="Service for extracting paragraphs from PDFs.",
     install_requires=requirements,
