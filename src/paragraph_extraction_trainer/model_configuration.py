d832e15ca18cc7378aa53c84be9641bcb2689b8c
Ali Altıparmak <ali@huridocs.org>
2023-09-12 16:12:20 +0300

    Add model configs

--- src/paragraph_extraction_trainer/model_configuration.py
+++ src/paragraph_extraction_trainer/model_configuration.py
@@ -1,18 +1,23 @@
 from pdf_tokens_type_trainer.ModelConfiguration import ModelConfiguration
 
 config_json = {
-    "num_class": 2,
-    "context_size": 1,
-    "num_boost_round": 1000,
-    "num_leaves": 149,
+    "boosting_type": "gbdt",
+    "objective": "binary",
+    "metric": "binary_logloss",
+    "verbose": -1,
     "learning_rate": 0.1,
-    "lambda_l1": 6.557567e-08,
-    "lambda_l2": 2.3948e-07,
-    "feature_fraction": 0.4,
-    "bagging_fraction": 1.0,
-    "bagging_freq": 0,
-    "min_data_in_leaf": 20,
+    "context_size": 1,
+    "num_boost_round": 400,
+    "num_leaves": 191,
+    "bagging_fraction": 0.9166599392739231,
+    "bagging_freq": 7,
+    "feature_fraction": 0.3116707710163228,
+    "lambda_l1": 0.0006901861637621734,
+    "lambda_l2": 1.1886914989632197e-05,
+    "min_data_in_leaf": 50,
+    "feature_pre_filter": True,
     "seed": 22,
+    "deterministic": True
 }
 
 MODEL_CONFIGURATION = ModelConfiguration(**config_json)
