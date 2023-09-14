ad7d07d1f589a7bbb49e79568161586284f6b571
Gabo <gabriel.piles.glez@gmail.com>
2023-09-14 13:57:58 +0200

    Run formatter

--- src/paragraph_extraction_trainer/download_models.py
+++ src/paragraph_extraction_trainer/download_models.py
@@ -3,17 +3,15 @@ from huggingface_hub import hf_hub_download
 paragraph_extraction_model_path = hf_hub_download(
     repo_id="HURIDOCS/pdf-segmetation",
     filename="paragraph_extraction_model.model",
-    revision="ffc4f1753ac1ca55981be06c29f1e73787e611b5"
+    revision="ffc4f1753ac1ca55981be06c29f1e73787e611b5",
 )
 
 letter_corpus_path = hf_hub_download(
-    repo_id="HURIDOCS/pdf-segmetation",
-    filename="letter_corpus.txt",
-    revision="da00a69c8d6a84493712e819580c0148757f466c"
+    repo_id="HURIDOCS/pdf-segmetation", filename="letter_corpus.txt", revision="da00a69c8d6a84493712e819580c0148757f466c"
 )
 
 toc_model_path = hf_hub_download(
     repo_id="HURIDOCS/pdf-segmetation",
     filename="TwoModelsV3SegmentsContext2Len.model",
-    revision="d66b49062439d6283464edadd9428a186c553f64"
+    revision="d66b49062439d6283464edadd9428a186c553f64",
 )
