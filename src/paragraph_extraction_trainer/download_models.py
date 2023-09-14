e2ba9ea2d87270de499f978ed8be380dc1cd4a35
Gabo <gabriel.piles.glez@gmail.com>
2023-09-14 12:06:15 +0200

    Fix imports for pipeline

--- src/paragraph_extraction_trainer/download_models.py
+++ src/paragraph_extraction_trainer/download_models.py
@@ -1,24 +1,24 @@
 from huggingface_hub import hf_hub_download
 
-from paragraph_extraction_trainer import trainer_config
+from paragraph_extraction_trainer.trainer_config import HUGGINGFACE_PATH
 
 paragraph_extraction_model_path = hf_hub_download(
     repo_id="HURIDOCS/pdf-segmetation",
     filename="paragraph_extraction_model.model",
     revision="ffc4f1753ac1ca55981be06c29f1e73787e611b5",
-    cache_dir=trainer_config.HUGGINGFACE_PATH,
+    cache_dir=HUGGINGFACE_PATH,
 )
 
 letter_corpus_path = hf_hub_download(
     repo_id="HURIDOCS/pdf-segmetation",
     filename="letter_corpus.txt",
     revision="da00a69c8d6a84493712e819580c0148757f466c",
-    cache_dir=trainer_config.HUGGINGFACE_PATH,
+    cache_dir=HUGGINGFACE_PATH,
 )
 
 toc_model_path = hf_hub_download(
     repo_id="HURIDOCS/pdf-segmetation",
     filename="TwoModelsV3SegmentsContext2Len.model",
     revision="d66b49062439d6283464edadd9428a186c553f64",
-    cache_dir=trainer_config.HUGGINGFACE_PATH,
+    cache_dir=HUGGINGFACE_PATH,
 )
