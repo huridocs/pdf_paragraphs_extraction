cb0d6beeeca5bcb176924c3035c74bfee95ce889
Gabo <gabriel.piles.glez@gmail.com>
2024-04-02 12:40:59 +0200

    Upgrade dependencies

--- setup.py
+++ setup.py
@@ -10,10 +10,11 @@ setup(
     name=PROJECT_NAME,
     packages=["paragraph_extraction_trainer"],
     package_dir={"": "src"},
-    version="0.27",
+    version="0.28",
     url="https://github.com/huridocs/pdf_paragraphs_extraction",
     author="HURIDOCS",
     description="Service for extracting paragraphs from PDFs.",
     install_requires=requirements,
+    setup_requieres=requirements,
     dependency_links=dependency_links,
 )
