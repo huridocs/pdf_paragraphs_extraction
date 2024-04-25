215c549d5be249b9127406ae1fc17b1f7537e7da
Gabo <gabriel.piles.glez@gmail.com>
2024-04-25 10:12:30 +0200

    Add license

--- foo.py
+++ foo.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     path = Path("requirements.txt")
     dependency_links = [r for r in path.read_text().splitlines() if r.startswith("git+")]
     requirements = [r for r in path.read_text().splitlines() if not r.startswith("git+")]
